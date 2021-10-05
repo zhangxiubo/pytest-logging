b63fa671cbac41ec7287c73ac8e6e302df065ca5
Xiubo Zhang <zhangxiubo@gmail.com>
2021-10-05 23:09:25 +0000

    Additional logging about sys.path.

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,5 +1,12 @@
+import logging
+import sys
 from pytest_logging.core import some_function
 
+logger = logging.getLogger(__name__)
+
 def test():
+    logger.info(f'__name__: {__name__}')
+    # note that somehow pytest is able to locate the acutal package with some sys.path magic:
+    logger.info(f'sys.path: {sys.path}')
     some_function('run1')
     some_function('run2')
